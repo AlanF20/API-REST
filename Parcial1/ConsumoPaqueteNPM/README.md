5ea83dfa839fc30b48c8e3b85b9fddce20ab11a7
Alan Flores <alanif@hotmail.com>
2023-08-29 19:25:54 -0500

    feat: adding parcial2

--- Parcial1/ConsumoPaqueteNPM/README.md
+++
@@ -1,4 +0,0 @@
-# Paquetes de npm 
-
-Practica para consumir el paquete creado y publicado en NPM mediante el require (commonjs) e import (ecsma)
-![Alt text](image.png)
--- Parcial1/ConsumoPaqueteNPM/src/utils/paqueteEcsma.js
+++
@@ -1,8 +0,0 @@
-import { alphanumeric, generateHashtag, rot13 } from "paquetesnpm";
-
-export function obtenerValoresEcsma(string) {
-  const alfanumeric = alphanumeric(string)
-  const hashtag = generateHashtag(string)
-  const encripted = rot13(string)
-  return { alfanumeric, hashtag, encripted }
-}
