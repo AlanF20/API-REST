1a0d34f74c7b99181c0de2d42a57a366d0b1a1ee
Alan Flores <alanif@hotmail.com>
2023-09-29 08:25:41 -0500

    fix: removing old repot

--- Parcial2/ConsumoPaqueteNPM/src/utils/paqueteRequire.js
+++
@@ -1,8 +0,0 @@
-import * as metodos from "../../simulacionPaqueteCommonJs/paquetenpmcommon.js"
-
-export function obtenerValoresCommon(string) {
-  const alfanumeric = metodos.default.alphanumeric(string)
-  const hashtag = metodos.default.generateHashtag(string)
-  const encripted = metodos.default.rot13(string)
-  return { alfanumeric, hashtag, encripted }
-}
--- Parcial2/ModulosNPM/README.md
+++
@@ -1,4 +0,0 @@
-# Generacion de archivos mediante modulos de Node
-
-En el proyecto se utilizo React y Reactpdf
-![Alt text](image.png)
